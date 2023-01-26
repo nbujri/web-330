6688f5a5fdc4a02c9e7150fcf702f1bcbc9985c9
Ngi Bujri <nbujri7@gmail.com>
2023-01-26 13:50:57 -0500

    setup files

---
+++ week-4/calorie-converter.js
@@ -0,0 +1,8 @@
+/*
+==========================================================
+    ; Title: Assignment 
+    ; Author: Ngi Bujri
+    ; Date: 26 January, 2023
+    ; Description: calorie app demonstrating static properties, built-in functions and lambda expressions
+==========================================================
+ */
