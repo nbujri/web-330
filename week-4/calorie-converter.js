e2dad72261a992636b4e2138670d323bd063a395
Ngi Bujri <nbujri7@gmail.com>
2023-01-28 20:53:44 -0500

    complete assignment 4.2

--- week-4/calorie-converter.js
+++ week-4/calorie-converter.js
@@ -6,3 +6,24 @@
     ; Description: calorie app demonstrating static properties, built-in functions and lambda expressions
 ==========================================================
  */
+
+import { FoodModel } from "./food-model.js";
+
+export class CalorieConverter {
+  static data = [
+    new FoodModel(1007, "Egg", 78),
+    new FoodModel(1008, "Apple", 95),
+    new FoodModel(1009, "Hamburger", 354),
+    new FoodModel(1010, "Fries", 400),
+    new FoodModel(1011, "Banana", 105),
+    new FoodModel(1012, "Soda", 150),
+  ];
+
+  // return array of item matching user input
+  static find(searchString) {
+    return this.data.filter((food) =>
+      // call toLowerCase for case sensitivity
+      food.name.toLowerCase().includes(searchString)
+    );
+  }
+}
--- week-4/food-model.js
+++ week-4/food-model.js
@@ -6,3 +6,11 @@
     ; Description: calorie app demonstrating static properties, built-in functions and lambda expressions
 ==========================================================
  */
+
+export class FoodModel {
+  constructor(id, name, calories) {
+    this.id = id;
+    this.name = name;
+    this.calories = calories;
+  }
+}
